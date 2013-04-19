cc072e9cf8671cf77af9b6e03e7f8a005dcb994a
MikeCoder <mikecoder13@gmail.com>
2013-04-19 14:47:58 +0800

    全部上传

---
+++ js/heart.js
@@ -0,0 +1,260 @@
+(function ($) {
+
+		    function Vector(x, y) {
+			this.x = x;
+			this.y = y;
+		    };
+		    Vector.prototype = {
+			rotate: function (theta) {
+			    var x = this.x;
+			    var y = this.y;
+			    this.x = Math.cos(theta) * x - Math.sin(theta) * y;
+			    this.y = Math.sin(theta) * x + Math.cos(theta) * y;
+			    return this;
+			},
+			mult: function (f) {
+			    this.x *= f;
+			    this.y *= f;
+			    return this;
+			},
+			clone: function () {
+			    return new Vector(this.x, this.y);
+			},
+			length: function () {
+			    return Math.sqrt(this.x * this.x + this.y * this.y);
+			},
+			subtract: function (v) {
+			    this.x -= v.x;
+			    this.y -= v.y;
+			    return this;
+			},
+			set: function (x, y) {
+			    this.x = x;
+			    this.y = y;
+			    return this;
+			}
+		    };
+
+		    function Petal(stretchA, stretchB, startAngle, angle, growFactor, bloom) {
+			this.stretchA = stretchA;
+			this.stretchB = stretchB;
+			this.startAngle = startAngle;
+			this.angle = angle;
+			this.bloom = bloom;
+			this.growFactor = growFactor;
+			this.r = 1;
+			this.isfinished = false;
+		    }
+		    Petal.prototype = {
+			draw: function () {
+			    var ctx = this.bloom.garden.ctx;
+			    var v1, v2, v3, v4;
+			    v1 = new Vector(0, this.r).rotate(Garden.degrad(this.startAngle));
+			    v2 = v1.clone().rotate(Garden.degrad(this.angle));
+			    v3 = v1.clone().mult(this.stretchA); //.rotate(this.tanAngleA);
+			    v4 = v2.clone().mult(this.stretchB); //.rotate(this.tanAngleB);
+			    ctx.strokeStyle = this.bloom.c;
+			    ctx.beginPath();
+			    ctx.moveTo(v1.x, v1.y);
+			    ctx.bezierCurveTo(v3.x, v3.y, v4.x, v4.y, v2.x, v2.y);
+			    ctx.stroke();
+			},
+			render: function () {
+			    if (this.r <= this.bloom.r) {
+			        this.r += this.growFactor; // / 10;
+			        this.draw();
+			    } else {
+			        this.isfinished = true;
+			    }
+			}
+		    }
+		    function Bloom(p, r, c, pc, garden) {
+			this.p = p;
+			this.r = r;
+			this.c = c;
+			this.pc = pc;
+			this.petals = [];
+			this.garden = garden;
+			this.init();
+			this.garden.addBloom(this);
+		    }
+		    Bloom.prototype = {
+			draw: function () {
+			    var p, isfinished = true;
+			    this.garden.ctx.save();
+			    this.garden.ctx.translate(this.p.x, this.p.y);
+			    for (var i = 0; i < this.petals.length; i++) {
+			        p = this.petals[i];
+			        p.render();
+			        isfinished *= p.isfinished;
+			    }
+			    this.garden.ctx.restore();
+			    if (isfinished == true) {
+			        this.garden.removeBloom(this);
+			    }
+			},
+			init: function () {
+			    var angle = 360 / this.pc;
+			    var startAngle = Garden.randomInt(0, 90);
+			    for (var i = 0; i < this.pc; i++) {
+			        this.petals.push(new Petal(Garden.random(Garden.options.petalStretch.min, Garden.options.petalStretch.max), Garden.random(Garden.options.petalStretch.min, Garden.options.petalStretch.max), startAngle + i * angle, angle, Garden.random(Garden.options.growFactor.min, Garden.options.growFactor.max), this));
+			    }
+			}
+		    }
+
+		    function Garden(ctx, element) {
+			this.blooms = [];
+			this.element = element;
+			this.ctx = ctx;
+		    }
+		    Garden.prototype = {
+			render: function () {
+			    for (var i = 0; i < this.blooms.length; i++) {
+			        this.blooms[i].draw();
+			    }
+			},
+			addBloom: function (b) {
+			    this.blooms.push(b);
+			},
+			removeBloom: function (b) {
+			    var bloom;
+			    for (var i = 0; i < this.blooms.length; i++) {
+			        bloom = this.blooms[i];
+			        if (bloom === b) {
+			            this.blooms.splice(i, 1);
+			            return this;
+			        }
+			    }
+			},
+			createRandomBloom: function (x, y) {
+			    this.createBloom(x, y, Garden.randomInt(Garden.options.bloomRadius.min, Garden.options.bloomRadius.max), Garden.randomrgba(Garden.options.color.min, Garden.options.color.max, Garden.options.color.opacity), Garden.randomInt(Garden.options.petalCount.min, Garden.options.petalCount.max));
+			},
+			createBloom: function (x, y, r, c, pc) {
+			    new Bloom(new Vector(x, y), r, c, pc, this);
+			},
+			clear: function () {
+			    this.blooms = [];
+			    this.ctx.clearRect(0, 0, this.element.width, this.element.height);
+			}
+		    }
+
+		    Garden.options = {
+			petalCount: {
+			    min: 9,
+			    max: 13
+			},
+			petalStretch: {
+			    min: 0.9,
+			    max: 3
+			},
+			growFactor: {
+			    min: 0.1,
+			    max: 1
+			},
+			bloomRadius: {
+			    min: 5,
+			    max: 10
+			},
+			density: 50,
+			growSpeed: 1000 / 30,
+			color: {
+			    min: 0,
+			    max: 90,
+			    opacity: 0.05
+			},
+			tanAngle: 90
+		    };
+		    Garden.random = function (min, max) {
+			return Math.random() * (max - min) + min;
+		    };
+		    Garden.randomInt = function (min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		    };
+		    Garden.circle = 2 * Math.PI;
+		    Garden.degrad = function (angle) {
+			return Garden.circle / 360 * angle;
+		    };
+		    Garden.raddeg = function (angle) {
+			return angle / Garden.circle * 360;
+		    };
+		    Garden.rgba = function (r, g, b, a) {
+			return 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+		    };
+		    Garden.randomrgba = function (min, max, a) {
+			return Garden.rgba(Math.round(Garden.random(0, 255)), Math.round(Garden.random(50, 255)), Math.round(Garden.random(80, 255)), 0.05);
+		    };
+
+		    $(function () {
+
+
+			// garden
+			var gardenCtx, gardenCanvas, $garden, garden, previewInterval;
+
+			// saveCanvas
+			var saveCtx, saveCanvas;
+
+			// setup save
+			saveCanvas = $("#garden")[0];
+			saveCanvas.width = $window.width();
+			saveCanvas.height = $window.height();
+			saveCtx = saveCanvas.getContext("2d");
+			saveCtx.globalCompositeOperation = "lighter";
+
+			// setup garden
+			$garden = $("#garden");
+			gardenCanvas = $garden[0];
+			gardenCanvas.width = $window.width();
+			gardenCanvas.height = $window.height() - 20;
+			gardenCtx = gardenCanvas.getContext("2d");
+			gardenCtx.globalCompositeOperation = "lighter";
+			garden = new Garden(gardenCtx, gardenCanvas);
+
+			// renderLoop
+			setInterval(function () {
+			    garden.render();
+			}, Garden.options.growSpeed);
+
+			//---------------------------------------------------------------
+			//Draw My Picture
+			var r = 18;
+			var time, temp = 500;
+			var radian = Math.PI;
+			var radianDecrement = Math.PI / 370 * 2;//
+			var heartX, heartY;
+			var length = 300, width = 300;
+			//画爱心
+			for(var i=0;i<354;i++){
+					time = 25*(i + temp);
+				 if(i <= 180&&i >=205) time=280*25;
+					setTimeout(drawHeart, time);
+			}
+			//画矩形
+			for(var i=0;i<354;i++){
+				length = 500 + i * 20;
+				width = 500 + i * 20;
+				setTimeout(draw, 50 * i + 2000);
+			}
+				
+ 
+			function drawHeart(){
+				heartX = getX(radian);
+				heartY = getY(radian);
+				radian += radianDecrement;
+				if(!(radian<=Math.PI+205*radianDecrement&&radian>=Math.PI+180*radianDecrement))//ֻ
+					garden.createRandomBloom(heartX, heartY);
+			}
+
+			function getX(t) {//
+				return 300+r*(16*Math.pow(Math.sin(t),3));
+			}
+			 
+			function getY(t) {//
+				return 300-r*(13*Math.cos(t)-5*Math.cos(2*t)-2*Math.cos(3*t)-Math.cos(4*t));
+			}
+			
+			function draw(){
+					garden.createRandomBloom(length, width);
+			}
+			//---------------------------------------------------------------
+		    });
+		})(jQuery);
---
+++ js/time.js
@@ -0,0 +1,36 @@
+function showTime() { 
+	var currentDate = new Date(); 
+	
+	var startDate = new Date(2010, 7, 19);
+	var date3 = currentDate-startDate;
+	//计算出相差天数
+	var days=Math.floor(date3/(24*3600*1000));
+	 
+	//计算出小时数
+	var leave1=date3%(24*3600*1000);     //计算天数后剩余的毫秒数
+	var hours=Math.floor(leave1/(3600*1000));
+	 
+	//计算相差分钟数
+	var leave2=leave1%(3600*1000);        //计算小时数后剩余的毫秒数
+	var minutes=Math.floor(leave2/(60*1000));
+	
+	//计算相差秒数
+	var leave3=leave2%(60*1000);          //计算分钟数后剩余的毫秒数
+	var seconds=Math.round(leave3/1000);
+	// 
+	//格式化输出 
+	// 
+	if (minutes < 10) 
+	minutes = "0" + minutes; 
+	if (seconds < 10) 
+	seconds = "0" + seconds; 
+	var currentTimeString = "Dear Nicole:<br>"
+		+"我们已经相恋: "+days+"天 "+hours+"小时 "+minutes+"分钟 "+seconds+"秒<br>"
+		+"未来还有更长的路,我相信我们能够继续走下去.<br>"
+		+"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp----Mike<br>"
+		+"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp----2013-5-15"; 
+	
+	document.getElementById("show").innerHTML=currentTimeString;  //改这地方
+	
+	window.setTimeout("showTime()", 1000); 
+} 
